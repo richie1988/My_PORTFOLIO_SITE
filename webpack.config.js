a35328f8d43a5b9398c43ad080f62f1d656a06ff
richie1988 <richiessikaonga@gmail.com>
2024-02-18 00:35:56 +0200

    React reveal errors handling and github pages

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,15 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
     ],
   },
 };
